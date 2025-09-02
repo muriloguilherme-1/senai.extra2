8b5537374a315a4aecfdf36135b83682c9f2b999
alexmedeirosdeveloper <axsousa97@gmail.com>
2025-09-02 17:34:27 -0300

    projeto java

---
+++ index.js
@@ -0,0 +1,22 @@
+let minhaVariavel = 10;
+console.log(minhaVariavel);
+
+minhaVariavel = "ola mundo!"
+console.log(minhaVariavel);
+
+minhaVariavel = true;
+console.log(minhaVariavel);
+
+let nome =  "murilo";
+let Nome = "paulo";
+let NOME = "laura";
+console.log(nome);
+console.log(NOME);
+
+let corFavorita = "azul";
+function mostrarCor() {
+    console.log (corFavorita);
+}
+mostrarCor(); //azul
+mostrarCor(); //azul
+
---
+++ programa.sequencial.js
@@ -0,0 +1,7 @@
+//entrada
+const n1 =Number(prompt("digitar o primeiro numero"));
+const n2 =Number(prompt("digitar o segundo numero"));
+// processando 
+const soma = n1 + n2;
+//saida
+console.log('A soma e ${soma.toFixed(2)}')

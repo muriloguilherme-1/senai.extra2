87e7db234a5f707942d7e1ef4bcb4c91e618b061
murilo guilherme silva <choelholilo7@gmail.com>
2025-09-10 15:02:04 -0300

    licao professor

--- programa.sequencial.js
+++ programa.sequencial.js
@@ -1,8 +1,55 @@
-const nums = [10,15,20,25,30];
-const Nums2 = nums.filter(n => n >= 20);
-console.log(Nums2);
+// const nums = [10,15,20,25,30];
+// const Nums2 = nums.filter(n => n >= 20);
+// console.log(Nums2);
 
-const palavras = ["js", "html"];
-const maiusculo = palavras.map
-const mudar =["palavras"]
-console.log (palavras)
+// const palavras = ["js", "html"];
+// const palavrasmaiusculo = palavras.map(palavras => palavras.
+//     toUpperCase()); 
+// console.log (palavrasmaiusculo)
+
+
+// const nomes = [
+//     {id: 1, nome: "A"},
+//     {id: 3, nome: "M"},
+//     {id: 2, nome: "L"},
+// ]
+// const usuarioid3 = usuarios.find(usuario => usuario.id === 3);
+// console.log (usuarioid3)
+
+// const produtos = [
+//     { nome: "X", preco: 10},
+//     { nome: "Y", preco: 30},
+// ];
+// const produtoscaros = produtos.filter(p => p.preco > 20);
+
+// const nomescaros = produtoscaros.map(p => p.nome);
+// console.log(produtoscaros);
+
+
+// const itens = [
+//     {preco: 10},
+//     {preco: 20},
+//     {preco: 5},
+    
+// ];
+// const precototal = itens.reduce((acumulador, item) => {
+//     return acumulador + item.preco;
+// }, 0)
+// console.log(precototal);
+
+const itens = [
+    { nome: "Caneta", categoria: "papel", preco: 3 },
+    { nome: "Caderno", categoria: "papel", preco: 15 },
+    { nome: "Banana", categoria: "alimento", preco: 2 },
+  ];
+  const agrupado = itens.reduce((acc, item) =>{
+    if (!acc[item.categoria]) {
+        acc[item.categoria] = 0
+    }
+    acc [item.categoria] += item.preco;
+    return acc;
+
+  },{})
+  console.log(agrupado)
+
+

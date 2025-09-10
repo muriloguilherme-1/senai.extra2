9e4779e6f65ea0d98b7d12e719948f13e3d30b83
murilo guilherme silva <choelholilo7@gmail.com>
2025-09-10 15:18:38 -0300

    .

--- programa.sequencial.js
+++ programa.sequencial.js
@@ -37,19 +37,36 @@
 // }, 0)
 // console.log(precototal);
 
+// const itens = [
+//     { nome: "Caneta", categoria: "papel", preco: 3 },
+//     { nome: "Caderno", categoria: "papel", preco: 15 },
+//     { nome: "Banana", categoria: "alimento", preco: 2 },
+//   ];
+//   const agrupado = itens.reduce((acc, item) =>{
+//     if (!acc[item.categoria]) {
+//         acc[item.categoria] = 0
+//     }
+//     acc [item.categoria] += item.preco;
+//     return acc;
+
+//   },{})
+//   console.log(agrupado)
+
+
+
+
 const itens = [
-    { nome: "Caneta", categoria: "papel", preco: 3 },
-    { nome: "Caderno", categoria: "papel", preco: 15 },
-    { nome: "Banana", categoria: "alimento", preco: 2 },
-  ];
-  const agrupado = itens.reduce((acc, item) =>{
-    if (!acc[item.categoria]) {
-        acc[item.categoria] = 0
-    }
-    acc [item.categoria] += item.preco;
-    return acc;
+ { nome: "Caneta", categoria: "papel", preco: 1000 },
+     { nome: "Caderno", categoria: "papel", preco: 500 },
+        { nome: "Banana", categoria: "alimento", preco: 350 },
+      ];
 
-  },{})
-  console.log(agrupado)
+      const produtos= produtos.filter(produtos => produtos.preco > 500);
 
+      const caderno = produtos.find(produto => produto.nome === "caderno");
 
+      const nomesprodutos = produtos.map(produtos => produtos.nome);
+
+      const totalpreco = produtos.reduce((acumulador, produto) => {
+        return acumulador + produto.preco;
+      }, 0)
